22e17ac03850810d02598d3c4fa4216420ee7a66
PoH <pr0f3ss0r.h@gmail.com>
2024-02-21 14:32:36 -0800

    collection and search done

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -6,6 +6,32 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        body: Padding(
+      padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
+      child: SafeArea(
+          child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            'Shoes\nCollection',
+            style: GoogleFonts.lunasima(
+                textStyle: const TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.w700,
+            )),
+          ),
+          Expanded(
+            child: TextField(
+              decoration: InputDecoration(
+                  hintText: 'Search',
+                  prefixIcon: const Icon(Icons.search_outlined),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(50))),
+            ),
+          )
+        ],
+      )),
+    ));
   }
 }

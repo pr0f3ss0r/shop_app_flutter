e81ebb2d211a9c3633a197089bca1134aaf861a3
PoH <pr0f3ss0r.h@gmail.com>
2024-02-21 14:58:47 -0800

    list view builder for filter done

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -4,31 +4,46 @@ import 'package:google_fonts/google_fonts.dart';
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  final List<String> filters = const ['All', 'Nike', 'Adidas', 'Prada'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Padding(
       padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
       child: SafeArea(
-          child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Column(
         children: [
-          Text(
-            'Shoes\nCollection',
-            style: GoogleFonts.lunasima(
-                textStyle: const TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.w700,
-            )),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Shoes\nCollection',
+                style: GoogleFonts.lunasima(
+                    textStyle: const TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.w700,
+                )),
+              ),
+              Expanded(
+                child: TextField(
+                  decoration: InputDecoration(
+                      hintText: 'Search',
+                      prefixIcon: const Icon(Icons.search_outlined),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(50))),
+                ),
+              ),
+            ],
           ),
-          Expanded(
-            child: TextField(
-              decoration: InputDecoration(
-                  hintText: 'Search',
-                  prefixIcon: const Icon(Icons.search_outlined),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(50))),
-            ),
+          SizedBox(
+            height: 120,
+            child: ListView.builder(
+                itemCount: filters.length,
+                itemBuilder: (context, index) {
+                  final filter = filters[index];
+                  Chip(label: Text(filter));
+                }),
           )
         ],
       )),
